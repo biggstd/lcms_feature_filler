56a84adef3c6df8570512996080d2da38a905ef5
Tyler Biggs <biggstd@gmail.com>
2023-01-31 11:06:56 -0800

    updated openms filter process

--- bin/openms_filefilter.sh
+++ bin/openms_filefilter.sh
@@ -3,4 +3,6 @@
 while IFS=, read -r sample_id feature_id rtmin rtmax mzmin mzmax
 do
     FileFilter -in $2 -out ${sample_id}__${feature_id}.mzML -mz ${mzmin}:${mzmax} -rt ${rtmin}:${rtmax} 
+    collect_features.py --mi-results ${mi_results} --sample-feature ${sample_id}__${feature_id}.mzML
+    rm ${sample_id}__${feature_id}.mzML
 done < $1
